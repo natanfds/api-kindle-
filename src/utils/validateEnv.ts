9e37245e14df71839fdf3c785b87f2528cf80673
natanfds <natanfernandesdossantos@zohomail.com>
2024-08-03 17:14:35 -0300

    [feat] Validação de env

---
+++ src/utils/validateEnv.ts
@@ -0,0 +1,9 @@
+import Joi from 'joi';
+
+export const ValidateEnv = Joi.object({
+  EMAIL_HOST: Joi.string().required(),
+  EMAIL_PORT: Joi.number().required(),
+  EMAIL_USER: Joi.string().required(),
+  EMAIL_PASS: Joi.string().required(),
+  EMAIL_FROM: Joi.string().required(),
+});
